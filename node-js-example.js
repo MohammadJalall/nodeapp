a2b80555b88febf10c56f0e14c9ead43e387f760
MohammadJalall <115039018+MohammadJalall@users.noreply.github.com>
2022-10-18 16:09:59 -0400

    Update node-js-example.js

--- node-js-example.js
+++ node-js-example.js
@@ -5,6 +5,6 @@ var http = require('http');
 http.createServer(function (req, res) {
     // write the response and send it to the client
     res.writeHead(200, {'Content-Type': 'text/html'}); 
-    res.write('JalalSay hello!');
+    res.write('JalalSay hi!');
     res.end();
 }).listen(9051);
